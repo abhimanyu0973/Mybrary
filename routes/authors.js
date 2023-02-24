9805e89d177e82419f6aba4f840e5b47c4a7ac44
abhimanyu gautam <abhimanyugautam10@gmail.com>
2023-02-24 16:54:47 +0530

    Books Index/Create/New

--- routes/authors.js
+++ routes/authors.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
     }
     try{
         const authors = await Author.find(searchOptions)
-        res.render('authors/index', {authors : authors, searchOption: searchOptions})
+        res.render('authors/index', {authors : authors, searchOptions: req.query})
     }
     catch{
         res.render('/')
